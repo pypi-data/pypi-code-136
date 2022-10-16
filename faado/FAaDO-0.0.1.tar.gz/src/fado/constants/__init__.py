b5ce6fc20c80057ccabb03e70368e78abe1ef9ba
Tom Forbes <tom@tomforb.es>
2022-10-16 12:18:35 +0000

    {"name":"faado","version":"0.0.1","file":"FAaDO-0.0.1.tar.gz","path":"faado/FAaDO-0.0.1.tar.gz"}

---
+++ faado/FAaDO-0.0.1.tar.gz/src/fado/constants/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+from fado import orchestrate, docker
+
+orchestrate_module_dir = os.path.dirname(orchestrate.__file__)
+docker_module_dir = os.path.dirname(docker.__file__)
+
+GENERAL_COMPOSE_FILE_PATH = os.path.join(orchestrate_module_dir, 'base_files', 'general_compose.yaml')
+BEN_CLIENT_PATH = os.path.join(docker_module_dir, 'benign-client')
+MAL_CLIENT_PATH = os.path.join(docker_module_dir, 'malicious-client')
+SERVER_PATH = os.path.join(docker_module_dir, 'server')
